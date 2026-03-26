9db457af50ccdafbe47f418155bcda10779b40c0
Roberto Izquierdo <robert1993_sira@hotmail.com>
2026-03-26 18:10:04 +0100

    modificaciones de archivos de configuración y añadido widget de formulario login

--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -1,4 +1,5 @@
-import 'package:app_3k_padel/features/widgets/boton_personalizado.dart';
+import 'package:app_3k_padel/features/widgets/custom_background.dart';
+import 'package:app_3k_padel/features/widgets/login_form.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,33 +10,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginState extends State<LoginScreen> {
-  String texto = "Prueba";
-
-  void pulsar() {
-    setState(() {
-      texto = "Pulsado el botón";
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image(image: AssetImage('assets/backgrounds/grupo_personas_padel.png')),
-          
-          Center(
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.green),
-                color: Colors.white,
-              ),
-              margin: EdgeInsets.all(30),
-              height: 300,
-              child: Center(child: Text("Prueba")),
-            ),
-          ),
-        ],
+      body: Fondo(
+        imagePath: 'assets/backgrounds/fondo_login.jpg',
+        child: LoginForm(),
       ),
     );
   }
