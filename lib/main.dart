cff44bd6b2348f366fe891027e0bfc5449e504a7
Roberto Izquierdo <robert1993_sira@hotmail.com>
2026-03-27 18:41:29 +0100

    añadida dependencia supabase y creación archivo configuracion

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app_3k_padel/features/auth/login_screen.dart';
+import 'package:app_3k_padel/features/auth/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
