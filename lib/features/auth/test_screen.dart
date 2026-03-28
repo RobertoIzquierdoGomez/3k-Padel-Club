eb93253a3ee19f700f0ab7b68cc8f3231a57a139
Roberto Izquierdo <robert1993_sira@hotmail.com>
2026-03-28 12:51:11 +0100

    añadido modelo de usuario

--- lib/features/auth/test_screen.dart
+++ lib/features/auth/test_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app_3k_padel/widgets/custom_background.dart';
 import 'package:app_3k_padel/main.dart';
+import 'package:app_3k_padel/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class TestScreen extends StatefulWidget {
@@ -11,7 +12,7 @@ class TestScreen extends StatefulWidget {
 
 class _LoginState extends State<TestScreen> {
   final user = supabase.auth.currentUser;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +23,18 @@ class _LoginState extends State<TestScreen> {
             decoration: BoxDecoration(color: Colors.white),
             width: 200,
             height: 200,
-            child: Center(child: Text(user?.email ?? "Invitado")),
+            child: Center(
+              child: Column(
+                children: [
+                  Text(user?.email ?? "Invitado"),
+                  CustomButton(
+                    text: "Logout",
+                    isLoading: false,
+                    onPressFunction: () async => await supabase.auth.signOut(),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
---
+++ lib/model/user_model.dart
@@ -0,0 +1,39 @@
+class UserModel {
+  final String idUsuario;
+  final String nombre;
+  final String apellidos;
+  final String correo;
+  final double nivel;
+  final bool tipoMiembro;
+  final String rol;
+  final bool cambiarPassword;
+
+  UserModel({required this.idUsuario, required this.nombre, required this.apellidos, required this.correo,required this.nivel, required this.tipoMiembro, required this.rol, required this.cambiarPassword});
+
+  factory UserModel.fromMap(Map<String, dynamic> json) {
+    return UserModel(
+      idUsuario: json['id_usuario'],
+      nombre: json['nombre'],
+      apellidos: json['apellidos'],
+      correo: json['correo'],
+      nivel: json['nivel'],
+      tipoMiembro: json['tipo_miembro'],
+      rol: json['rol'],
+      cambiarPassword: json['cambiar_password'],
+    ); 
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'id_usuario': idUsuario,
+      'nombre': nombre,
+      'apellidos': apellidos,
+      'correo': correo,
+      'nivel': nivel,
+      'tipo_miembro': tipoMiembro,
+      'rol': rol,
+      'cambiar_password': cambiarPassword,
+    };
+  }
+}
+
