64e629f6824f861bbb98c2711a1425b76fb7ca9c
Roberto Izquierdo <robert1993_sira@hotmail.com>
2026-04-11 18:42:22 +0200

    añadido modelo para clases

---
+++ lib/model/clases_model.dart
@@ -0,0 +1,83 @@
+
+import 'package:app_3k_padel/model/user_model.dart';
+import 'package:intl/intl.dart';
+
+class ClasesModel {
+  final String idClase;
+  final String diaSemana;
+  final String horaInicio;
+  final String horaFin;
+  final bool estadoClase;
+  final List<UserModel> usuarios;
+
+  ClasesModel({
+    required this.idClase,
+    required this.diaSemana,
+    required this.horaInicio,
+    required this.horaFin,
+    required this.estadoClase,
+    required this.usuarios
+  });
+
+
+  static String _parseHora(dynamic hora) {
+    if (hora == null) return "";
+
+    final str = hora.toString().trim();
+
+    if (str.length >= 5) {
+      return str.substring(0, 5);
+    }
+
+    return str;
+  }
+
+  factory ClasesModel.fromJson(Map<String, dynamic> json){
+    return ClasesModel(
+      idClase: (json['id_clase'] as String?) ?? "",
+      diaSemana: (json['dia_semana'] as String?) ?? "",
+      horaInicio: _parseHora(json['hora_inicio']),
+      horaFin: _parseHora(json['hora_fin']),
+      estadoClase: json['estado_clase'] ?? true,
+      usuarios: (json['asignacion_clase'] as List<dynamic>? ?? [])
+          .map((e) => UserModel.fromJson(e['usuario']))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'id_clase': idClase,
+      'dia_semana': diaSemana,
+      'hora_inicio': horaInicio,
+      'hora_fin': horaFin,
+      'estado_clase': estadoClase
+    };
+  }
+
+  String get horaInicioFormateada {
+    final partes = horaInicio.split(':');
+    final dateTime = DateTime(
+      0,
+      1,
+      1,
+      int.parse(partes[0]),
+      int.parse(partes[1]),
+    );
+
+    return DateFormat('HH:mm').format(dateTime);
+  }
+
+  String get horaFinFormateada {
+    final partes = horaFin.split(':');
+    final dateTime = DateTime(
+      0,
+      1,
+      1,
+      int.parse(partes[0]),
+      int.parse(partes[1]),
+    );
+
+    return DateFormat('HH:mm').format(dateTime);
+  }
+}
