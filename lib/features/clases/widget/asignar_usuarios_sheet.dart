4b130ee9f674b5f5ba04354ae01214c676de39d6
Roberto Izquierdo <robert1993_sira@hotmail.com>
2026-04-12 16:32:19 +0200

    añadido la gestión de clase

--- lib/features/clases/widget/asignar_usuarios_sheet.dart
+++ lib/features/clases/widget/asignar_usuarios_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:app_3k_padel/model/user_model.dart';
-import 'package:app_3k_padel/widgets/custom_badge.dart';
 import 'package:app_3k_padel/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
