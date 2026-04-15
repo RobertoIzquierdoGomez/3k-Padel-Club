3bf3ef7442b472db67c9964098436d81fbe7e5dc
Roberto Izquierdo <robert1993_sira@hotmail.com>
2026-04-15 21:34:37 +0200

    modificada pantalla de login para hacerlo más responsive en web movil

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -5,32 +5,40 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
+    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: Fondo(
         imagePath: 'assets/backgrounds/fondo_login.jpg',
-        child: Column(
-          children: [
-            Image(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.only(bottom: keyboardHeight),
+            child: Column(
+              children: [
+                Image(
                   image: AssetImage('assets/logo/3k_logo_sin_fondo.PNG'),
                   width: 200,
                 ),
-            LoginForm(),
-            Wrap(
-              alignment: WrapAlignment.center,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              spacing: 40,
-              children: [
-                Image(image: AssetImage('assets/logo/haltera.png'), width: 80),
-                Image(image: AssetImage('assets/logo/is vet.jpeg'), width: 80),
-                Image(image: AssetImage('assets/logo/orto-nipace.jpg'), width: 80),
-                Image(image: AssetImage('assets/logo/oyma.png'), width: 80),
-                Image(image: AssetImage('assets/logo/stihl.png'), width: 80),
+                LoginForm(),
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 40,
+                  runSpacing: 10,
+                  children: [
+                    Image(image: AssetImage('assets/logo/haltera.png'), width: 80),
+                    Image(image: AssetImage('assets/logo/is vet.jpeg'), width: 80),
+                    Image(image: AssetImage('assets/logo/orto-nipace.jpg'), width: 80),
+                    Image(image: AssetImage('assets/logo/oyma.png'), width: 80),
+                    Image(image: AssetImage('assets/logo/stihl.png'), width: 80),
+                  ],
+                ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
